e18915eb328b155f2f35d33713eea9012b13284b
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-08-23 23:16:19 -0300

    Variaveis usadas para Player, Inimigo e Game

---
+++ src/settings.py
@@ -0,0 +1,25 @@
+# CFG DA SCREEM
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+# CFG DO PLAYER
+PLAYER_SPEED = 1
+PLAYER_IMG =  'assets/imagens/playerShip1_blue.png'
+PLAYER_LASER = "assets/imagens/laserBlue01.png"
+PLAYER_TIME_SPAW = 3000
+
+
+# CFG DO ENEMY
+ENEMY_SPEED = 1
+ENEMY_1 =  'assets/imagens/enemyRed4.png'
+ENEMY_2 =  'assets/imagens/enemyRed2.png'
+BOOS =  'assets/imagens/enemyRed5.png'
+ROCKET =  'assets/imagens/laserRed13.png'
+
+# cfg Game
+MUSIC_END = './assets/sounds/1x game over.ogg'
+MUSIC_WON = './assets/sounds/1x level win.ogg'
+MUSIC_GAME = './assets/sounds/01 game-game_0.ogg'
+
+
